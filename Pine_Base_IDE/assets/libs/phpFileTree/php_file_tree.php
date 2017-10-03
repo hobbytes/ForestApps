b4a3cb0aac52533bbc588e68471256797786afa8
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2017-10-03 12:25:25 +0300

    secure

--- Pine_Base_IDE/assets/libs/phpFileTree/php_file_tree.php
+++ Pine_Base_IDE/assets/libs/phpFileTree/php_file_tree.php
@@ -1,19 +1,9 @@
 <?php
 /*
-	
-	== PHP FILE TREE ==
-	
-		Let's call it...oh, say...version 1?
-	
 	== AUTHOR ==
-	
+
 		Cory S.N. LaViska
 		http://abeautifulsite.net/
-		
-	== DOCUMENTATION ==
-	
-		For documentation and updates, visit http://abeautifulsite.net/notebook.php?article=21
-		
 */
 
 
@@ -27,27 +17,38 @@ function php_file_tree($directory, $return_link, $extensions = array()) {
 
 function php_file_tree_dir($directory, $return_link, $extensions = array(), $first_call = true) {
 	// Recursive function called by php_file_tree() to list directories/files
-	
+	session_start();
 	// Get and sort directories/files
 	if( function_exists("scandir") ) $file = scandir($directory); else $file = php4_scandir($directory);
 	natcasesort($file);
 	// Make directories first
 	$files = $dirs = array();
 	foreach($file as $this_file) {
-		if( is_dir("$directory/$this_file" ) ) $dirs[] = $this_file; else $files[] = $this_file;
-	}
+		if( is_dir("$directory/$this_file" ) ){
+			if(eregi('system/users/',"$directory/$this_file") || eregi('system/core',"$directory/$this_file")){
+				if($_SESSION['superuser'] != $_SESSION['loginuser'] && !eregi('system/users/'.$_SESSION['loginuser'],"$directory/$this_file") || $_SESSION['superuser'] != $_SESSION['loginuser'] && eregi('system/core',"$directory/$this_file")){
+			}else{
+				$dirs[] = $this_file;
+			}
+		}else{
+			$dirs[] = $this_file;
+		}
+		}else{
+			$files[] = $this_file;
+		}
+	 }
 	$file = array_merge($dirs, $files);
-	
+
 	// Filter unwanted extensions
 	if( !empty($extensions) ) {
 		foreach( array_keys($file) as $key ) {
 			if( !is_dir("$directory/$file[$key]") ) {
-				$ext = substr($file[$key], strrpos($file[$key], ".") + 1); 
+				$ext = substr($file[$key], strrpos($file[$key], ".") + 1);
 				if( !in_array($ext, $extensions) ) unset($file[$key]);
 			}
 		}
 	}
-	
+
 	if( count($file) > 2 ) { // Use 2 instead of 0 to account for . and .. "directories"
 		$php_file_tree = "<ul";
 		if( $first_call ) { $php_file_tree .= " class=\"php-file-tree\""; $first_call = false; }
@@ -62,7 +63,7 @@ function php_file_tree_dir($directory, $return_link, $extensions = array(), $fir
 				} else {
 					// File
 					// Get extension (prepend 'ext-' to prevent invalid classes from extensions that begin with numbers)
-					$ext = "ext-" . substr($this_file, strrpos($this_file, ".") + 1); 
+					$ext = "ext-" . substr($this_file, strrpos($this_file, ".") + 1);
 					$link = str_replace("[link]", "$directory/" . urlencode($this_file), $return_link);
 					$php_file_tree .= "<li class=\"pft-file " . strtolower($ext) . "\"><a href=\"$link\">" . htmlspecialchars($this_file) . "</a></li>";
 				}
