cb6371f1b399d8ff0100fa580127350556bc9e94
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2017-10-12 12:36:07 +0300

    уменьшение трафика 30%

--- Wood_Pecker/main.php
+++ Wood_Pecker/main.php
@@ -3,7 +3,7 @@
 $appname=$_GET['appname'];
 $appid=$_GET['appid'];
 ?>
-<div id="<?echo $appname.$appid;?>" style="background-color:#444753; height:500px; width:600px; border-radius:0px 0px 5px 5px; overflow:hidden;">
+<div id="<?echo $appname.$appid;?>" style="background-color:#444753; height:540px; width:600px; border-radius:0px 0px 5px 5px; overflow:hidden;">
 <?php
 /*--------Подключаем библиотеки--------*/
 include '../../core/library/etc/security.php';
@@ -44,15 +44,19 @@ if(!is_file($contacts_file)){
   file_put_contents($contacts_file,'['.$_SESSION['loginuser'].']');
 }
 
-$new_contact = $_GET['new_contact'];
+$new_contact  = $_GET['new_contact'];
+$check_user   = file_get_contents('http://forest.hobbytes.com/media/os/ubase/checkuser.php?check='.$new_contact);
 $get_contacts = file_get_contents($contacts_file);
- if(!empty($new_contact)){
+ if(!empty($new_contact) && $check_user == 'true'){
    $this_user = "[".$_SESSION['loginuser']."]";
    if(!eregi($this_user, $get_contacts)){
      file_put_contents($contacts_file,$this_user."\r\n".$get_contacts."\r\n");
    }
    $get_contacts = str_replace($this_user,$this_user."\r\n".$new_contact.'=',$get_contacts);
    file_put_contents($contacts_file,$get_contacts);
+ }
+ elseif(!empty($new_contact) && $check_user == 'false'){
+   $wp_gui->errorLayot('user not found: <b>'.$new_contact.'</b>');
  }
 
 $history_file = parse_ini_file($chat_file,TRUE);
@@ -70,37 +74,76 @@ $folder=$_GET['destination'];
 /*--------Логика--------*/
 ?>
 <style>
-.msgbubble{
+.wp_msgbubble{
   padding:5px;
   display:block;
   width:60%;
   margin:-2px 21px;
   line-height: 26px;
 }
+.wp_msgbubble_own{
+  float:right;
+  text-align:right;
+}
+.wp_msgbubble_other{
+  float:left;
+  text-align:left;
+}
 .wp_contacts{
-  width:100%;
-  padding:10px;
+  width:auto;
+  padding:15px;
   text-align: left;
   font-size: 20px;
   background: #4b5169;
   border-bottom: 2px solid #5d6277;
   cursor:pointer;
   font-variant: all-small-caps;
+  word-wrap: break-word;
+}
+.wp_message_info{
+  font-size: 12px;
+  color:#878787;
+  padding:0 10px;
+}
+.wp_message_info b{
+  font-variant:all-small-caps;
+  font-size:15px;
+}
+.wp_msg_bubble{
+  width:auto;
+  min-width:30%;
+  max-width:90%;
+  color:#fff;
+  padding:10px;
+  border-radius:10px;
+}
+.wp_msg_bubble_own{
+  background:#8bc34a;
+  float:right;
+}
+.wp_msg_bubble_error{
+  background:#e85b50;
+  float:right;
+}
+.wp_msg_bubble_other{
+  background:#94c2ed;
+  float:left;
 }
 </style>
 <div style="width:100%; height:100%; min-height:400px; min-width:600px;">
-  <div id="users<?echo $appid?>" style="min-height:442px; background: #444753; color:#fff; width:30%; float:left; height:100%;">
+  <div id="users<?echo $appid?>" style="min-height:442px; background: #444753; color:#fff; width:30%; float:left; height:100%; overflow:auto; overflow-x:hidden;">
     <?
     foreach ($contacts_file[$_SESSION['loginuser']] as $key => $value){
 
       echo $$contacts_file[$_SESSION['loginuser']][$key];
-      echo '<div id="wp_'.$key.'" onclick="wp_load'.$appid.'('."'wp_sel_user'".',this.id)" class="wp_contacts">'.$key.'</div>';
+      echo '<div id="wp_'.$key.'" onclick="wp_load'.$appid.'('."'wp_sel_user'".',this.id)" class="wp_contacts ui-forest">'.$key.'</div>';
     }
     ?>
     <input id="wp_newcontact<?echo $appid?>" style="margin:10px; width:90%; padding:10px; background:#ececec; border:none;" type="text" />
     <div id="wp_addcontactbtn<?echo $appid?>" onclick="wp_add<?echo $appid?>('<?echo $wp_sel_user?>')" class="ui-forest-button ui-forest-cancel ui-forest-center" style="width:80%;">add</div>
   </div>
   <div id="messagebox<?echo $appid?>" style="min-height:400px; background: #ececec; width:70%; height:100%; float:right;">
+    <div style="background:#dcdcdc; padding:3px; text-align:center; border-bottom:1px solid #ccc; color:#4c4b4b; box-shadow: 1px 1px 4px #ccc;"><?echo 'Chat with: <b>'.$wp_sel_user.'</b>';?></div>
     <div id="messages<?echo $appid?>" style="min-height:300px; word-break: break-word; padding:5px; height:70%; overflow:auto; overflow-x:hidden;">
       <?
       foreach ($history_file[$wp_sel_user] as $key => $value){
@@ -108,18 +151,24 @@ $folder=$_GET['destination'];
         $time = str_replace(array('_d','t_','_'),array('','',':'),stristr(stristr($date,'d_'),'t_'));
         $date = str_replace(array('d_','_','_t'),array('','.',''),stristr(stristr($date,'_t',true),'d_'));
         if(!eregi('own',$key)){
-          echo '<div class="msgbubble" style="float:left; text-align:left;">
-          <div style="font-size: 12px; color:#878787; padding:0 10px;"><b style="font-variant:all-small-caps; font-size:15px;">'.$wp_sel_user.'</b> '.$date.', '.$time.'</div>
-          <div style="width:100%; color:#fff; background:#94c2ed; padding:10px; border-radius:10px;">
-          '.$history_file[$wp_sel_user][$key].'
-          </div>
+          echo '
+          <div class="wp_msgbubble wp_msgbubble_other">
+            <div class="wp_message_info">
+              <b>'.$wp_sel_user.'</b> '.$date.', '.$time.'
+            </div>
+            <div class="wp_msg_bubble wp_msg_bubble_other">
+              '.$history_file[$wp_sel_user][$key].'
+            </div>
           </div>';
         }else{
-        echo '<div class="msgbubble" style="float:right; text-align:right;">
-        <div style="font-size: 12px; color:#878787; padding:0 10px;"><b style="font-variant:all-small-caps; font-size:15px;">you</b> '.$date.', '.$time.'</div>
-        <div style="width:100%; color:#fff; background:#8bc34a; padding:10px; border-radius:10px;">
-        '.$history_file[$wp_sel_user][$key].'
-        </div>
+        echo '
+        <div class="wp_msgbubble wp_msgbubble_own">
+          <div class="wp_message_info">
+            <b>you</b> '.$date.', '.$time.'
+          </div>
+          <div class="wp_msg_bubble wp_msg_bubble_own">
+            '.$history_file[$wp_sel_user][$key].'
+          </div>
         </div>';
         }
       }
@@ -135,25 +184,30 @@ $folder=$_GET['destination'];
 <script>
 /*--------Логика JS--------*/
 function wp_load<?echo $appid;?>(key,value){
-$("#<?echo $appid;?>").load("<?echo $folder;?>/main.php?"+key+"="+value+"&id=<?echo rand(0,10000).'&appid='.$appid.'&mobile='.$click.'&appname='.$appname.'&dir='.realpath($entry).'&destination='.$folder;?>")
+  clearInterval(timerId);
+  $("#<?echo $appid;?>").load("<?echo $folder;?>/main.php?"+key+"="+value+"&id=<?echo rand(0,10000).'&appid='.$appid.'&mobile='.$click.'&appname='.$appname.'&dir='.realpath($entry).'&destination='.$folder;?>")
 };
 
-$("#wp_<?echo $wp_sel_user?>").css('background','#8a4231');
+$("#wp_<?echo $wp_sel_user?>").css('background','#6a98fd');
 function wp_send<?echo $appid;?>(value){
   if(value){
+    var msg_content = $("#sendinput<?echo $appid?>").text();
     $.ajax({
       type: "POST",
       url: "<?echo $folder;?>sender",
       data: {
-         sm:$("#sendinput<?echo $appid?>").text(),
+         sm:msg_content,
          tu:value,
          o:'own'
       },
       success: function(data){
         status_send = data.replace(/^\s*/,'').replace(/\s*$/,'');
         if (status_send == 'true'){
-          clearInterval(timerId);
-          $("#<?echo $appid;?>").load("<?echo $folder;?>/main.php?to_user="+value+"&id=<?echo rand(0,10000).'&appid='.$appid.'&mobile='.$click.'&appname='.$appname.'&dir='.realpath($entry).'&destination='.$folder;?>");
+          wp_newmessage(msg_content,'_own','_own');
+          $("#sendinput<?echo $appid?>").html('');
+        }else{
+          wp_newmessage('#error: message is not delivered','_error','_own');
+          $("#sendinput<?echo $appid?>").html('');
         }
       }
     }).done(function(o) {
@@ -161,9 +215,20 @@ function wp_send<?echo $appid;?>(value){
   }
 };
 
+function wp_newmessage(message_content,type,owner){
+  var today = new Date();
+  var date_time = $.datepicker.formatDate("d.m.y, ", today) + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+  var messageinfo = $("<div/>").addClass("wp_message_info").html("<b>you</b> " + date_time);
+  var message = $("<div/>").addClass("wp_msg_bubble wp_msg_bubble" + type).html(message_content);
+  var messagebox = $("<div/>").addClass("wp_msgbubble wp_msgbubble" + owner).html(message);
+  messagebox.prepend(messageinfo);
+  $("#messages<?echo $appid?>").prepend(messagebox);
+}
+
 function wp_add<?echo $appid;?>(){
   var new_c = $("#wp_newcontact<?echo $appid;?>").val();
   if(new_c){
+    clearInterval(timerId);
     $("#<?echo $appid;?>").load("<?echo $folder;?>/main.php?new_contact="+new_c+"&id=<?echo rand(0,10000).'&appid='.$appid.'&mobile='.$click.'&appname='.$appname.'&dir='.realpath($entry).'&destination='.$folder;?>");
   }
 };
