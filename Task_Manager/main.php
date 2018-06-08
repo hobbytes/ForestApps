8cc3d65c7449f8cf8ef5bf51ff9beab9fec7b5a4
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2018-06-08 11:15:26 +0300

    теперь показывает колличество трафика каждой программы + поменял иконку

--- Task_Manager/main.php
+++ Task_Manager/main.php
@@ -53,6 +53,14 @@ $language  = parse_ini_file('app.lang');
   font-weight: 900;
   font-size: 13px;
 }
+
+.trmngr{
+	background: #f5f5f5;
+}
+
+.trmngr:nth-child(even){
+	background: #e0e0e0;
+}
 </style>
 <div>
   <table border='1' cellpadding="7" style="border-collapse: collapse; border:1px solid #d4d4d4; width:100%; text-align: center;">
@@ -76,14 +84,13 @@ var temp_id = 0;
 var new_id  = 0;
 var new_name = '';
 var new_loc = '';
-var color<?echo $AppID?> = '#f5f5f5';
 $(".process-container").each(function(index, element){
-  let p_id = $(element).attr("id");
-  let p_name = $("#drag"+p_id + "> .process-title").text();
-  let p_loc = $(element).attr("location");
-	let _applength = $("#app" + p_id).attr("applength-" + p_id);
-	let p_applength = (_applength / 1024).toPrecision(3) + " KB";
-  $("#taskcontainer<?echo $AppID?>").append('<tr style="background:'+color<?echo $AppID?>+';" t_id="'+p_id+'" t_applength="'+_applength+'" id="task'+p_id+'"><td>'+p_name+'</td><td>'+p_id+'</td><td>'+p_applength+'</td><td class="tm-box-left""><span onClick="open_folder('+p_id+')">'+p_loc+'</span><div class="tm-box-close ui-forest-blink" onClick="task_close('+p_id+'); checkwindows();">x</div></td></tr>');
+  var p_id = $(element).attr("id");
+  var p_name = $("#drag"+p_id + "> .process-title").text();
+  var p_loc = $(element).attr("location");
+	var _applength = $("#app" + p_id).attr("applength-" + p_id);
+	var p_applength = (_applength / 1024).toPrecision(3) + " KB";
+  $("#taskcontainer<?echo $AppID?>").append('<tr class="trmngr" t_id="'+p_id+'" t_applength="'+_applength+'" id="task'+p_id+'"><td>'+p_name+'</td><td>'+p_id+'</td><td style="transition:all 0.3s ease;" id="length-container-'+p_id+'">'+p_applength+'</td><td class="tm-box-left""><span onClick="open_folder('+p_id+')">'+p_loc+'</span><div class="tm-box-close ui-forest-blink" onClick="task_close('+p_id+'); checkwindows();">x</div></td></tr>');
   temp_id = p_id;
 });
 
@@ -97,18 +104,23 @@ function task_check<?echo $AppID;?>(){
   });
   if(new_id > temp_id){
     temp_id = new_id;
-		if(color<?echo $AppID?> == '#f5f5f5'){
-			color<?echo $AppID?> = '#e0e0e0';
-		}else{
-			color<?echo $AppID?> = '#f5f5f5';
-		}
-    $("#taskcontainer<?echo $AppID?>").append('<tr style="background:'+color<?echo $AppID?>+'; t_id="'+new_id+'" t_applength="'+_applength+'" id="task'+new_id+'"><td>'+new_name+'</td><td>'+new_id+'</td><td>'+new_applength+'</td><td class="tm-box-left"><span onClick="open_folder('+new_id+')">'+new_loc+'</span><div class="tm-box-close ui-forest-blink" onClick="task_close('+new_id+'); checkwindows();">x</div></td></tr>');
+
+    $("#taskcontainer<?echo $AppID?>").append('<tr class="trmngr" t_id="'+new_id+'" t_applength="'+_applength+'" id="task'+new_id+'"><td>'+new_name+'</td><td>'+new_id+'</td><td style="transition:all 0.3s ease;" id="length-container-'+new_id+'">'+new_applength+'</td><td class="tm-box-left"><span onClick="open_folder('+new_id+')">'+new_loc+'</span><div class="tm-box-close ui-forest-blink" onClick="task_close('+new_id+'); checkwindows();">x</div></td></tr>');
   }
   $("#taskcontainer<?echo $AppID?> > tr").each(function(index, element){
     var get_id = $(element).attr("t_id");
+		_al = $("#app" + get_id).attr("applength-" + get_id);
     if(!$("#process" + get_id).length){
       $("#task"+get_id).remove();
     }
+		if(_al > $("#task" + get_id).attr('t_applength')){
+			$("#length-container-" + get_id).css('color','#2196F3');
+			$("#length-container-" + get_id).html((_al / 1024).toPrecision(3) + " KB");
+			$("#task" + get_id).attr('t_applength', _al);
+		}else{
+			$("#length-container-" + get_id).css('color','#000');
+		}
+		_al = null;
 
   });
 }
@@ -125,11 +137,11 @@ folder = '<?echo $_SERVER['DOCUMENT_ROOT']?>/'+folder+'';
 makeprocess('<?echo $_SERVER['DOCUMENT_ROOT'].'/system/apps/Explorer/main.php'?>',folder,'dir','<?echo $language[$_SESSION['locale'].'_explorer_title']?>');
 }
 
-var timer<?echo $AppID;?> = setInterval(function(){
+var timer<?echo $AppID?> = setInterval(function(){
   if($("#<?echo $AppName.$AppID;?>").length){
-    task_check<?echo $AppID;?>();
+    task_check<?echo $AppID?>();
 }else{
-  clearInterval(timer<?echo $AppID;?>);
+  clearInterval(timer<?echo $AppID?>);
 }
 },500);
 </script>
