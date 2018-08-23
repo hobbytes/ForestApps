47d22b4e9414e1e9aef285b464f4fe4026d55109
Vyacheslav Gorodilov <gvgsochi@gmail.com>
2018-08-23 15:19:37 +0300

    не помню что сделал

--- Firefox_OS_Apps_Support/main.php
+++ Firefox_OS_Apps_Support/main.php
@@ -19,13 +19,14 @@ $AppContainer->AuthorInfo = 'Forest Media';
 /* Container Info */
 $AppContainer->appName = $AppName;
 $AppContainer->appID = $AppID;
-$AppContainer->LibraryArray = array('gui', 'permissions');
+$AppContainer->LibraryArray = array('gui', 'permissions', 'filesystem');
 $AppContainer->height = '100%';
 $AppContainer->width = '100%';
 $AppContainer->customStyle = 'padding-top:0px;';
 $AppContainer->StartContainer();
 
 
+$faction = new fileaction;
 $newpermission = new PermissionRequest;
 $object = new gui;
 $newpermission->fileassociate(array('webapp'), $Folder.'main.php', 'foxloader', $AppName);
@@ -46,15 +47,24 @@ if(isset($_GET['foxloader'])){
 	$json = json_decode(file_get_contents('http://'.$_SERVER['SERVER_NAME'].$file), true);
 }
 
+//echo array_shift($json['icons']);
+
+
+
 if(!empty($file)){
 	$launch_path = $json['launch_path'];
-	$dir = 'http://'.$_SERVER['SERVER_NAME'].str_replace(array('manifest.webapp', $_SERVER['DOCUMENT_ROOT']),'',$file).$launch_path;
-	echo '<iframe width="100%" height="100%" class="app-cointainer'.$AppID.'" style="border: 0;" src="'.$dir.'"></iframe>';
+	$file = str_replace('manifest.webapp', '', $file).$launch_path;
+	$file = $faction->filehash($_SERVER['DOCUMENT_ROOT'].$file, 'false');
+	$file = str_replace($_SERVER['DOCUMENT_ROOT'], '', $file);
+
+	$dir = 'http://'.$_SERVER['SERVER_NAME'].$file;
+	echo '<iframe id="frame'.$AppID.'" class="app-cointainer'.$AppID.'" style="border: 0; width: inherit; height: inherit; display:block;" src="'.$dir.'"></iframe>';
 }
 
 $AppContainer->EndContainer();
 ?>
 <script>
 /*--------JS Logic--------*/
+$('#frame<? echo $AppID ?>').find('html').css('height','100%');
 
 </script>
