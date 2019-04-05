3c9815462b35cca8e8c1e0378dc5449ae7179b85
VG <gvgsochi@gmail.com>
2019-04-05 12:47:04 +0300

    работаем

--- Audio_Player/main.php
+++ Audio_Player/main.php
@@ -15,11 +15,11 @@ $AppContainer = new AppContainer;
 /* App Info */
 $AppContainer->AppNameInfo = 'Audio Player'; // app name information @string
 $AppContainer->SecondNameInfo = 'Audio Player'; // second app name information @string
-$AppContainer->VersionInfo = '1.0';  // app version @string
+$AppContainer->VersionInfo = '1.0.1';  // app version @string
 $AppContainer->AuthorInfo = 'Forest Media'; // app version @string
 
 /* Library List */
-$AppContainer->LibraryArray = array('gui', 'permissions');
+$AppContainer->LibraryArray = array('gui', 'permissions', 'filesystem');
 
 /* Container Info */
 $AppContainer->appName = $AppName;
@@ -44,17 +44,21 @@ if(empty($musicloader)){
 
 $NewPermission = new PermissionRequest;
 $object = new gui;
+$fileaction = new fileaction;
 $NewPermission->fileassociate(array('mp3', 'wav', 'ogg', 'aac'), $Folder.'main.php', 'musicloader', $AppName);
 
+$style_link = $Folder.$fileaction->filehash('css/audioplayer.css','false');
+
+echo '<link rel="stylesheet" href="'.$style_link.'">';
+
 ?>
-<link rel="stylesheet" href="<? echo $Folder ?>/css/audioplayer.css?h=1" />
 
 <style>
-<?echo "#wrapper$AppID" ?> { margin: 10px auto; max-width: 400px; width: 400px; height: 100px;}
+<?echo "#wrapper$AppID" ?> { margin: 10px auto; min-width: 400px; min-height: 100px; max-width: 84%; width: 84%; overflow: hidden; }
 </style>
 
 <div id="<?echo "wrapper$AppID" ?>">
-  <div style="text-align: center; color: #272727; font-size: 20px; word-break: break-word;">
+  <div style="text-align: center; color: #272727; margin-bottom: 10px; font-size: 20px; word-break: break-word;">
     <? echo basename(str_replace('_', ' ', $musicloader)) ?>
   </div>
   <audio id="<?echo "audio$AppID" ?>" preload="auto" autoplay="<? echo $autoplay ?>" controls>
